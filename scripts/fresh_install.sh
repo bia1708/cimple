c3db2d3344090c5becc33e598e573ba30e15065d
bia1708 <bianca.popu@gmail.com>
2024-03-03 22:15:04 +0200

    script/fresh_install.sh: Add user creation
    Signed-off-by: bia1708 <bianca.popu@gmail.com>

--- scripts/fresh_install.sh
+++ scripts/fresh_install.sh
@@ -3,12 +3,34 @@
 username=$1
 password=$2
 
+if [ $(which jenkins) ]; then
+	echo "Jenkins already exists on this device."
+	exit 1
+fi
+
+# Install java
 sudo apt update
-sudo apt install java-11-openjdk
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo apt install -y curl fontconfig openjdk-17-jre
+
+# Install jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
 sudo apt install jenkins
 
-sudo systemctl daemon reload
+# Enable service
+sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 
+# Start jenkins
 sudo systemctl start jenkins &
+
+# Auth for the first time
+sudo wget http://192.168.1.219:8080/jnlpJars/jenkins-cli.jar
+pat=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+#java -jar jenkins-cli.jar -s http://192.168.1.219:8080 -auth admin:$pat
+
+echo "jenkins.model.Jenkins.instance.securityRealm.createAccount('\""$username"\"', '\""$password"\"')" | java -jar jenkins-cli.jar -auth admin:$pat -s http://192.168.1.219:8080/ groovy =
