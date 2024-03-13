eba72af38435308ef1a279d5e96902944a999993
bia1708 <bianca.popu@gmail.com>
2024-03-13 22:00:15 +0200

    Fix install bug and add setup wizard bypass
    Signed-off-by: bia1708 <bianca.popu@gmail.com>

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from service.configurator import Configurator
 
 
 if __name__ == '__main__':
-    config = Configurator("")
+    config = Configurator()
+    config.perform_fresh_install("bia", "1234")
     # config.connect_to_existing_jenkins("bia", "1234", "http://localhost:8080")
     print(config.get_current_server())
     
