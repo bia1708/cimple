b4b2fe7fb07ef659a6d02b5ff7c8a465ea3af6e8
bia1708 <bianca.popu@gmail.com>
2024-03-14 22:44:03 +0200

    Try npm install fix
    Signed-off-by: bia1708 <bianca.popu@gmail.com>

--- src/scripts/enable_proxy.sh
+++ src/scripts/enable_proxy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Install smee dependencies (one time)
+packages=$(npm list)
+# if [ echo $packages | grep]
 npm install --global node@latest
 npm install --global smee-client
 
 tunnel=$(curl -X GET https://smee.io/new | awk '{print $5}')
-echo $tunnel
+echo "tunnel:$tunnel"
+
 
 smee --url $tunnel --path /github-webhook/ --port 8080
