51c808c0efa31b19e8fd3db4c0040b46860136c1
bia1708 <bianca.popu@gmail.com>
2024-04-08 10:50:56 +0300

    install_plugins.sh: Fix disable script security
    Signed-off-by: bia1708 <bianca.popu@gmail.com>

--- src/scripts/server_configuration/install_plugins.sh
+++ src/scripts/server_configuration/install_plugins.sh
@@ -20,7 +20,7 @@ java -jar $jnlp -s $url -auth $username:$pat restart
 systemctl stop jenkins
 # echo '[Service]\nJAVA_ARGS="-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false  -Dpermissive-script-security.enabled=true"' > /etc/systemd/system/jenkins.service.d/override.conf
 sed -i 's/Environment="JAVA_OPTS=-Djava.awt.headless=true"/Environment="JAVA_OPTS=-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false"/' /lib/systemd/system/jenkins.service
-sed -i 's,    <useScriptSecurity>true</useScriptSecurity>,    <useScriptSecurity>false</useScriptSecurity>,' /var/lib/jenkins/javaposse.jobdsl.plugin.GlobalJobDslSecurityConfiguration.xml
+sed -i 's,<useScriptSecurity>true</useScriptSecurity>,<useScriptSecurity>false</useScriptSecurity>,' /var/lib/jenkins/javaposse.jobdsl.plugin.GlobalJobDslSecurityConfiguration.xml
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
