904485e977c866abe1e8513fd56ca3b1c7b05b30
bia1708 <bianca-maria.popu@analog.com>
2024-04-08 14:30:07 +0300

    Fix services and security disable
    Signed-off-by: bia1708 <bianca-maria.popu@analog.com>

--- src/scripts/server_configuration/enable_proxy.sh
+++ src/scripts/server_configuration/enable_proxy.sh
@@ -11,7 +11,7 @@ tunnel=$(curl -X GET https://smee.io/new | awk '{print $5}')
 echo "tunnel:$tunnel"
 
 # Create systemd service for smee webhook forwarding
-service_script=$'
+service_script=$"
 [Unit]
 Description=Smee Proxy
 After=network.target jenkins.service
@@ -25,7 +25,7 @@ User=jenkins
 ExecStart=smee --url $tunnel --path /github-webhook/ --port 8080
 
 [Install]
-WantedBy=multi-user.target'
+WantedBy=multi-user.target"
 
 echo "$service_script" > /etc/systemd/system/smee.service
 
