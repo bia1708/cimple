7d348d8e1c95a5136f548d7307659fec3bb3522e
bia1708 <bianca.popu@gmail.com>
2024-04-23 20:50:27 +0300

    Add job table and Create button + layout
    Signed-off-by: bia1708 <bianca.popu@gmail.com>

--- src/ui/components/list_view.py
+++ src/ui/components/list_view.py
@@ -6,17 +6,27 @@ from PySide6.QtCore import QSize
 class ListView(QListView):
     def __init__(self, parent=None):
         super(ListView, self).__init__(parent)
+        
+        self.setMouseTracking(True)
 
-        self.setFixedWidth(170)
         self.setStyleSheet(
             """
             QListView {
                 background-color: #2E2E2E;
                 color: white;
                 border: 1px solid #444444;
+                font-size: 16px;
+            }
+
+            QListView {
+                background-color: #2E2E2E;
+                color: white;
+                border: 1px solid #444444;
+                font-size: 16px;
             }
 
             QListView::item {
+                height: 25px;
                 padding: 5px;
                 background-color: #2E2E2E;
             }
@@ -29,7 +39,6 @@ class ListView(QListView):
 
             QListView::item:hover {
                 padding: 5px;
-
                 border: 0.5px solid #81B622;
                 background-color: #2E2E2E;
                 color: #ECF87F;
