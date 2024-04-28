7b260f2fd9f2622ece967d5751de5aa71827826d
bia1708 <bianca.popu@gmail.com>
2024-04-29 00:12:06 +0300

    Finish add job functionality
    Signed-off-by: bia1708 <bianca.popu@gmail.com>

--- src/scripts/job_configuration/check_auth.sh
+++ src/scripts/job_configuration/check_auth.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git_token=$1
+username=$2
 
 echo $git_token > ../artifacts/token.txt
 output=$(gh auth login --with-token < ../artifacts/token.txt 2>&1 | grep error)
@@ -9,6 +10,12 @@ rm ../artifacts/token.txt
 if [ ! -z "$output" ]; then
     echo "Authentication error. Please check your git credentials and try again."
     exit 1
+fi
+
+user_info=$(curl -H "Authorization: token $git_token" https://api.github.com/user)
+if [ "$(echo $user_info | grep "\"$username\"")" == "" ]; then
+    echo "Username and token don't match!"
+    exit 2
 fi
 
 gh auth status
